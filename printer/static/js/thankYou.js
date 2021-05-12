af362255da22cb81c13bf0f4d604f8644e7f49a1
Andrew <healeycodes@gmail.com>
2021-05-12 08:52:31 +0100

    Fix JS var naming

--- printer/static/js/thankYou.js
+++ printer/static/js/thankYou.js
@@ -3,14 +3,14 @@ const animation = document.querySelector("#animation");
 
 animation.innerHTML = "waiting for printer";
 function checkStatus() {
-  anim.innerHTML += "\n...";
-  fetch(`check-msg?msg_id=${msgid}`)
+  animation.innerHTML += "\n...";
+  fetch(`check-msg?msg_id=${msgID}`)
     .then((res) => res.json())
     .then((json) => {
       if (!json.status.includes("printed")) {
         setTimeout(checkStatus, 400);
       } else {
-        anim.innerHTML =
+        animation.innerHTML =
           json.status + '<p><a href="/"><button>Print another</button></a></p>';
       }
     });
