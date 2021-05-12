a21536c44d1189d902966c1be5d565e3249dfca8
Andrew <healeycodes@gmail.com>
2021-05-12 09:54:48 +0100

    Fix thank you animation

--- printer/static/js/thankYou.js
+++ printer/static/js/thankYou.js
@@ -3,14 +3,14 @@ const animation = document.querySelector("#animation");
 
 animation.innerHTML = "waiting for printer";
 function checkStatus() {
-  animation.innerHTML += "\n...";
+  animation.innerHTML += "<br>...";
   fetch(`check-msg?msg_id=${msgID}`)
     .then((res) => res.json())
     .then((json) => {
       if (!json.status.includes("printed")) {
-        setTimeout(checkStatus, 400);
+        setTimeout(checkStatus, 500);
       } else {
-        animation.innerHTML =
+        animation.innerHTML +=
           json.status + '<p><a href="/"><button>Print another</button></a></p>';
       }
     });
