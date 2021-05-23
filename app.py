95febe38a92b7f5b60efba1d6c36d4dd6226a964
Andrew <healeycodes@gmail.com>
2021-05-23 18:07:36 +0100

    Refactor auth

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+import os
 from config import CONFIG
-from flask import Flask, redirect
-
+from functools import wraps
+from flask import Flask, redirect, abort, request
 
 app = Flask(__name__)
+
+
+def auth(view):
+    @wraps(view)
+    def check_pw(*args, **kwargs):
+        # TODO: this is vulnerable to a timing attack
+        password = request.args.get("password")
+        PW = os.environ["PRINTER_PW"] if "PRINTER_PW" in os.environ else None
+        if password != PW:
+            abort(401)
+        else:
+            return view(*args, **kwargs)
+
+    return check_pw
+
 
 if CONFIG.printer:
     from mods.printer import printer
