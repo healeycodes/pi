a95714c0008c2439dd112cf16b39f1c15bf9cd0f
Andrew <healeycodes@gmail.com>
2021-05-24 13:10:10 +0100

    Mixed up temperature and humidity oops

--- client/weather.py
+++ client/weather.py
@@ -26,7 +26,7 @@ def weather_thread(URL, PW):
         # try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
         while True:
-            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+            temperature, humidity = Adafruit_DHT.read_retry(sensor, pin)
             if humidity is not None and temperature is not None:
                 return [humidity, temperature]
             else:
