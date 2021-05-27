7a84b065756c636769e8121e26441bcdc5debd9d
Andrew <healeycodes@gmail.com>
2021-05-27 07:03:24 +0100

    Remove async/await

--- mods/home/static/js/index.js
+++ mods/home/static/js/index.js
@@ -2,63 +2,65 @@ const store = {};
 
 if (window.mods.includes("weather")) {
   const weather = document.querySelector("#weather");
-  async function update() {
-    const data = await fetch("/weather/get");
-    const json = await data.json();
-
-    const m = moment(json.timestamp);
-    m.add(1, "hours"); // Add offset
-    weather.innerHTML = `Weather data fresh as of ${m.fromNow()}..<br />
-    <ul>
-      <li>Temperature: ${json.temperature.toFixed(1)}°C</li>
-      <li>Humidity: ${json.humidity.toFixed(1)}°C</li>
-    </ul>`;
+  function update() {
+    fetch("/weather/get")
+      .then((data) => data.json())
+      .then((json) => {
+        const m = moment(json.timestamp);
+        m.add(1, "hours"); // Add offset
+        weather.innerHTML = `Weather data fresh as of ${m.fromNow()}..<br />
+<ul>
+  <li>Temperature: ${json.temperature.toFixed(1)}°C</li>
+  <li>Humidity: ${json.humidity.toFixed(1)}°C</li>
+</ul>`;
+      });
   }
   update();
 }
 
 if (window.mods.includes("printer")) {
   const printer = document.querySelector("#printer");
-  async function update() {
-    const data = await fetch("/printer/count-msgs");
-    const json = await data.json();
-
-    printer.innerHTML = `${json.msg_count} messages have been printed so far.`;
+  function update() {
+    fetch("/printer/count-msgs")
+      .then((data) => data.json())
+      .then((json) => {
+        printer.innerHTML = `${json.msg_count} messages have been printed so far.`;
+      });
   }
   update();
 }
 
 if (window.mods.includes("sky")) {
   const sky = document.querySelector("#sky");
-  async function update() {
-    const data = await fetch("/sky/get");
-    const json = await data.json();
+  function update() {
+    fetch("/sky/get")
+      .then((data) => data.json())
+      .then((json) => {
+        let lastSeenMinDate = new Date(0);
+        json.map((satellite) => {
+          lastSeenMinDate = Math.max(
+            lastSeenMinDate,
+            new Date(satellite.timestamp)
+          );
+        });
+        const m = moment(lastSeenMinDate);
+        m.add(1, "hours"); // Add offset
+        sky.innerHTML = `Satellite data fresh as of ${m.fromNow()}..<br /><ul>`;
 
-    let lastSeenMinDate = new Date(0);
-    json.map((satellite) => {
-      lastSeenMinDate = Math.max(
-        lastSeenMinDate,
-        new Date(satellite.timestamp)
-      );
-    });
-    const m = moment(lastSeenMinDate);
-    m.add(1, "hours"); // Add offset
-    sky.innerHTML = `Satellite data fresh as of ${m.fromNow()}..<br />
-    <ul>`;
+        // Visible satellites first
+        json.sort((sat1, sat2) => sat2.status - sat1.status);
+        json.forEach((satellite) => {
+          let color = "";
+          if (satellite.status === 1) {
+            color = "green";
+          } else {
+            color = "red";
+          }
 
-    // Visible satellites first
-    json.sort((sat1, sat2) => sat2.status - sat1.status);
-    json.forEach((satellite) => {
-      let color = "";
-      if (satellite.status === 1) {
-        color = "green";
-      } else {
-        color = "red";
-      }
-
-      sky.innerHTML += `<li style="color: ${color};">${satellite.description}</li>`;
-    });
-    sky.innerHTML += "</ul>";
+          sky.innerHTML += `<li style="color: ${color};">${satellite.description}</li>`;
+        });
+        sky.innerHTML += "</ul>";
+      });
   }
   update();
 }
